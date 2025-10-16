49ef74098b0ef333d2017354a033e32002cc4bcb
danielsangronis1386 <daniel.sangronis1386@gmail.com>
2025-10-16 12:40:50 -0700

    main app setting

---
+++ drnlog/drnlog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.urls import reverse
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+    duration = models.PositiveBigIntegerField(help_text="Duration in minutes")
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('workout_detail', kwargs={'pk': self.id})
+    
