5620c44d4a31e23fbf0f61f1b0f28a4902b6cc81
danielsangronis1386 <daniel.sangronis1386@gmail.com>
2025-10-18 23:12:36 -0700

    fixing 404

--- drnlog/workouts/admin.py
+++ drnlog/workouts/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from . models import Workout
+from . models import Workout, Category
 
 @admin.register(Workout)
 class WorkoutAdmin(admin.ModelAdmin):
     list_display = ('name', 'date', 'duration')
     search_fields = ('name',)
-    
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
--- drnlog/workouts/urls.py
+++ drnlog/workouts/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),  
+  
   path ('workouts/', views.workout_list, name='workout_list'),
-  path ('<int:pk>/', views.workout_detail, name='workout_detail'),
   path ('new/', views.workout_create, name='workout_create'),
+  path ('<int:pk>/', views.workout_detail, name='workout_detail'),
   path('<int:pk>/edit/', views.workout_update, name='workout_update'),
   path('<int:pk>/delete/', views.workout_delete, name='workout_delete'),
 
