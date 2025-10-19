0701a2a1cfd6fe71367c4abfa24540fa40dc7b5b
danielsangronis1386 <daniel.sangronis1386@gmail.com>
2025-10-18 22:47:55 -0700

    category setup

--- drnlog/workouts/models.py
+++ drnlog/workouts/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 from django.urls import reverse
 
+#Category
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+
 class Workout(models.Model):
     name = models.CharField(max_length=100)
     date = models.DateField(auto_now_add=True)
     duration = models.PositiveIntegerField(help_text="Duration in minutes")
     notes = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="workouts", null=True, blank=True)
 
     def __str__(self):
         return self.name
