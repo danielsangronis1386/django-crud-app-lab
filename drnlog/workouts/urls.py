fb9b0f8c564165c6d9e54116cc416214489941e4
danielsangronis1386 <daniel.sangronis1386@gmail.com>
2025-10-22 18:55:54 -0700

    add third model workout/url.py

--- drnlog/workouts/urls.py
+++ drnlog/workouts/urls.py
@@ -11,5 +11,10 @@ urlpatterns = [
   path('<int:workout_id>/exercise/new/', views.exercise_create, name='exercise_create'),
   path('exercise/<int:pk>/edit/', views.exercise_update, name='exercise_update'),
   path('exercise/<int:pk>/delete/', views.exercise_delete, name='exercise_delete')
+  path('muscleparts/', views.musclepart_list, name='musclepart_list'),
+  path('muscleparts/new/', views.musclepart_create, name='musclepart_create'),
+  path('muscleparts/<int:pk>/', views.musclepart_detail, name='musclepart_detail'),
+  path('muscleparts/<int:pk>/edit/', views.musclepart_update, name='musclepart_update'),
+  path('muscleparts/<int:pk>/delete/', views.musclepart_delete, name='musclepart_delete'),
 
 ]
